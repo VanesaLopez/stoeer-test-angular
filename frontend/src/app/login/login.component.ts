868027bc7ca218c9bca62b2c44356e74dde140bd
VanesaLopez <avanesalopez@hotmail.com>
2020-02-29 00:31:56 +0100

    Subscription management on login component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit {
-
-  public user: SocialUser;
-  public loggedIn: boolean;
+export class LoginComponent implements OnInit, OnDestroy {
+  loggedIn: boolean;
+  private subcriptions: Subscription = new Subscription();
 
   constructor(
     private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    this.authService.authState.subscribe((user) => {
-      this.user = user;
+    const sub = this.authService.authState.subscribe((user) => {
       this.loggedIn = (user != null);
     });
+
+    this.subcriptions.add(sub);
+  }
+
+  ngOnDestroy(): void {
+    this.subcriptions.unsubscribe();
   }
 
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
+
   signOut(): void {
     this.authService.signOut();
   }
