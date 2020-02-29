2e80bb664752bdd7dee8310f0bd40ff50c26e4fb
VanesaLopez <avanesalopez@hotmail.com>
2020-02-29 17:11:26 +0100

    Added api call methos

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,11 +2,36 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { User } from '../models/user.model';
 
 @Injectable()
 export class UserService {
 
-  private ulr = `${environment.apiURL}/api/users`;
+  public userList$: Observable<Array<User>> = new Observable<Array<User>>();
+  private url = `${environment.apiURL}/api/users`;  
 
   constructor(private httpClient: HttpClient) { }
+
+  getUserList(): void {
+    this.userList$ = this.httpClient.get<Array<User>>(this.url).pipe(
+      map(users => users.map(user => new User(user))),
+      catchError(err => {
+        return new Observable<Array<User>>();
+      })
+    );
+  }
+
+  deleteUser(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.url}/id`);
+  }
+
+  addUser(user: User): Observable<any> {
+    return this.httpClient.post(this.url, user);
+  }
+
+  modifyUser(user: User): Observable<any> {
+    return this.httpClient.patch(`${this.url}/${user.id}`, user);
+  }
+  
 }
