a111a54306d6e95e763a90b572fe63453d679dc1
VanesaLopez <avanesalopez@hotmail.com>
2020-02-29 12:32:56 +0100

    Changed imports & added interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { CommonModule } from '@angular/common';
-import { NavComponent } from './nav/nav.component';
-import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
 import { provideConfig } from './config/social-login';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HeaderInterceptor } from './interceptors/header.interceptor';
 
 @NgModule({
   declarations: [
@@ -28,6 +30,11 @@ import { provideConfig } from './config/social-login';
     {
       provide: AuthServiceConfig,
       useFactory: provideConfig
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HeaderInterceptor,
+      multi: true,
     }
   ],
   bootstrap: [AppComponent]
