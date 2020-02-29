def0e3174350272090d99480efc51d478911fc43
VanesaLopez <avanesalopez@hotmail.com>
2020-02-29 18:39:42 +0100

    Fixed auth-guard

--- frontend/src/app/services/auth-guard.service.ts
+++ frontend/src/app/services/auth-guard.service.ts
@@ -1,19 +1,32 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { AuthService } from 'angularx-social-login';
+import {CanActivate, Router} from '@angular/router';
+import {AuthService} from 'angularx-social-login';
+import {Observable, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {map, tap, catchError} from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
+  private isLoggedIn: boolean;
+
   constructor(
-    private authService: AuthService
-  ) { }
-
-  canActivate(): boolean {
-    this.authService.authState.subscribe((user) => {
-      return (user != null);
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.authService.authState.subscribe((currentUser) => {
+      this.isLoggedIn = currentUser !== null;
     });
+  }
 
-    return false;
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.authState.pipe(
+      map(user => user !== null),
+      catchError(err => of(false)),
+      tap((isLoggedIn) => {
+        if(!isLoggedIn) {
+          this.router.navigate(['']);
+        }
+      })
+    );
   }
 }
