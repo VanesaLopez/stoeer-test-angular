d027a1a88f4cbefc01d79f7eac3a7e3ddb505f2d
VanesaLopez <avanesalopez@hotmail.com>
2020-02-29 11:22:24 +0100

    Added login tests

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,7 +1,9 @@
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { AuthService, AuthServiceConfig, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
+import { AuthService, AuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
+import { By } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 
 const config = new AuthServiceConfig([
   {
@@ -40,5 +42,54 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('subscription', () => {
+    const addSpy = spyOn(Subscription.prototype, 'add');
+
+    component.ngOnInit();
+
+    expect(addSpy).toHaveBeenCalled();
+  });
+
+  it('should be login', () => {
+    component.loggedIn = true;
+    fixture.detectChanges();
+    const out = fixture.debugElement.query(By.css('.btn-out'));
+
+    expect(out).toBeTruthy();
+  });
+
+  it('should be logout', () => {
+    fixture.detectChanges();
+    const up = fixture.debugElement.query(By.css('.btn-up'));
+
+    expect(up).toBeTruthy();
+  });
+
+  it('sign with Google', () => {
+    const authService = TestBed.get(AuthService);
+    const signInSpy = spyOn(authService, 'signIn');
+
+    component.signInWithGoogle();
+
+    expect(signInSpy).toHaveBeenCalled();
+  });
+
+  it('signOut', () => {
+    const authService = TestBed.get(AuthService);
+    const signOutSpy = spyOn(authService, 'signOut');
+
+    component.signOut();
+
+    expect(signOutSpy).toHaveBeenCalled();
+  });
+
+  it('unsubscribe', () => {
+    const unsubscribeSpy = spyOn(Subscription.prototype, 'unsubscribe');
+
+    component.ngOnDestroy();
+
+    expect(unsubscribeSpy).toHaveBeenCalled();
   });
 });
