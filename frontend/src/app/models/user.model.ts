0ecc23d5b02905943c266b33734476b480ac0623
VanesaLopez <avanesalopez@hotmail.com>
2020-03-01 12:36:26 +0100

    Changed user model

--- frontend/src/app/models/user.model.ts
+++ frontend/src/app/models/user.model.ts
@@ -6,10 +6,10 @@ export class User {
     iban: string;
 
     constructor(data?) {
-        this.id = data.id ? data.id : null;
-        this.creator = data.creator ? data.creator : null;
-        this.first_name = data.first_name ? data.first_name : null;
-        this.last_name = data.last_name ? data.last_name : null;
-        this.iban = data.iban ? data.iban : null;
+        this.id = data && data.id ? data.id : null;
+        this.creator = data && data.creator ? data.creator : '';
+        this.first_name = data && data.first_name ? data.first_name : '';
+        this.last_name = data && data.last_name ? data.last_name : '';
+        this.iban = data && data.iban ? data.iban : '';
     }
 }
